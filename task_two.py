224e9d76ee06a3493bb6342975cd5cf55ba77488
88warren <warren_laura@yahoo.co.uk>
2024-03-05 20:39:21 +0000

    Task Two Complete

--- task_two.py
+++ task_two.py
@@ -1,3 +1,8 @@
+# Data Storage
+# Objective:
+# Store the retrieved data locally, simulating basic data persistence without the need for a full-fledged database.
+
+
 import requests
 import json
 
@@ -22,13 +27,23 @@ def display_todos(todos):
     else:
         print("No todos to display.")
 
+
+# The save_to_file(data, filename) function takes data and a filename as input and saves the data to a JSON file 
+# with the specified filename.
 def save_to_file(data, filename):
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
     print(f"Data saved to '{filename}'.")
 
+
+# In the __main__ block, after retrieving the todos data using get_todos(), the script checks if the data is not None.
+#  If todos data is retrieved successfully, it calls save_to_file(todos, "todos.json") to save the data to a file named "todos.json". 
+# Then, it proceeds to display the todos data using display_todos(todos).
 if __name__ == "__main__":
     todos = get_todos()
     if todos:
         save_to_file(todos, "todos.json")
         display_todos(todos)
+
+ 
+# In the console type: python task_two.py it will then save the database to a new file called todos.json
